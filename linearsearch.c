2bb9bf172576cfa76f78302e510a8cd8b10e6175
ryan7786 <fireash70@gmail.com>
2023-01-12 19:54:03 +0530

    linear search fixed

--- linearsearch.c
+++ linearsearch.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
  int linearsearch( int arr[] , int size, int key) {
+    int flag=0;
+    int location;
     for ( int i = 0; i<size; i++) {
         if ( arr[i]  == key ) {
-            printf("key is present");
-            return i;
-        } else {
-            printf(" not presnt");
+            flag=1;
+            location=i;
+           break;
         }
+    }
+    if(flag==1){
+        printf("Present! At location %d",location+1);
+    }else{
+        printf("Not present!");
     }
  }
  int main() {
   
   int array[6] = { 4,7,3,2,8,9};
   //Added new comment
-  linearsearch( array , 6, 1);
+  linearsearch( array , 6, 2);
     return 0;
-
 }
